2f0e6f2b74681e34741fb1576776e1339ab45429
Max-Foreback <4backsspace@gmail.com>
2023-12-08 13:17:17 -0500

    re-add config

---
+++ configz.py
@@ -0,0 +1,16 @@
+CONFIG = {
+    "world_x": 10, 
+    "world_y": 10, 
+    "swarm_size": 3, 
+    "num_swarms": 100, 
+    "num_generations": 1000,
+    "num_eval_timesteps": 20, 
+    "homogeneous": True, 
+    "num_observe": 20, 
+    "mutation_rate": .01, 
+    "mutation_magnitude": 1, 
+    "replace": True
+}
+
+resources = [0, 1]
+probs = [.5, .5]
